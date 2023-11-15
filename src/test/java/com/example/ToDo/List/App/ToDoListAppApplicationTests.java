223942fe873aa7ae824f7e5720ab794b31ec1b5d
Aravind <Aravind@BRN0122COKL117>
2023-11-15 11:48:16 +0530

    first initial project

---
+++ src/test/java/com/example/ToDo/List/App/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToDo.List.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
